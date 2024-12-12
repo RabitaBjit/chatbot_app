46bedb0b7addc121c42eb2330ca51171a7f9ddf9
Rabita Amin <rabita.amin@bjitgroup.com>
2024-12-12 12:11:16 +0600

    test2

--- src/chatbotEmbed.js
+++ src/chatbotEmbed.js
@@ -11,13 +11,12 @@
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
-  iframe.style.width = "350px";
-  iframe.style.height = "500px";
+  iframe.style.width = "64px";
+  iframe.style.height = "60px";
   iframe.style.border = "none";
   iframe.style.borderRadius = "10px";
   iframe.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.3)";
   iframe.style.zIndex = "9999";
-  iframe.style.display = "none";
   // Append the iframe to the body of the document
   document.body.appendChild(iframe);
 })();
